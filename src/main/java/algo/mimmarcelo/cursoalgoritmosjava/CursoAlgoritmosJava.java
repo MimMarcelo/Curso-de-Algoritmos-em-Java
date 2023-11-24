30988bd776787b9202c0dccd799fe3c4c244ba63
Marcelo Júnior <rokermarcelo@gmail.com>
2023-11-24 14:17:33 -0300

    Criação do projeto no NetBeans

---
+++ src/main/java/algo/mimmarcelo/cursoalgoritmosjava/CursoAlgoritmosJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package algo.mimmarcelo.cursoalgoritmosjava;
+
+/**
+ *
+ * @author marcelo
+ */
+public class CursoAlgoritmosJava {
+
+    public static void main(String[] args) {
+        System.out.println("Curso de algoritmos em Java!");
+        System.out.println("Por: Marcelo Júnior!");
+    }
+}
